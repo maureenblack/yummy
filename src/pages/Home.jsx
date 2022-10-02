b7893a0bbba9424343b68b9f6b13d4f3c31d4e1c
maureenblack <maureenwepngong@gmail.com>
2022-10-02 11:37:22 +0100

    added pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+
+function Home() {
+  return (
+    <div>
+      <Veggie></Veggie>
+      <Popular></Popular>
+    </div>
+  );
+}
+export default Home;
