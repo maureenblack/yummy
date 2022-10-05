a91b5acd3136964c6b5efeef0ffb3fd89881af85
maureenblack <maureenwepngong@gmail.com>
2022-10-05 12:48:33 +0100

    added images and slide

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,8 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+import {Splide, SplideSlide} from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function Popular() {
     const [popular, setPopular] = useState([]);
@@ -13,19 +16,41 @@ function Popular() {
     console.log(JSON.stringify(data))
     setPopular(data.categories);
 };
-
+   /* <div key={category.idCategory}>
+     <p> {category.strCategory}</p>*/
 
 return( 
  <div>
-    {popular.map((category) => {
         return(
-            <div key={category.idCategory}>
-              <p> {category.strCategory}</p>
-            </div>
+            <Wrapper>
+            <h3>Popular Picks</h3>
+            <Splide>
+            {popular.map((category) =>{
+                return(
+                    <SplideSlide> 
+                    <Card> 
+                    <p>{category.strCategory}</p>
+    <img src={category.strCategoryThumb} alt={category.strCategory} />
+                    </Card>
+                    </SplideSlide>
+                );
+            })}
+            </Splide>
+            </Wrapper>
         );
-    })}
-
     </div>
  );
 }
+const Wrapper = styled.div`
+margin: 4rem 0rem;
+`;
+const Card = styled.div`
+min-height: 25rem;
+border-radius: 2rem;
+overflow: hidden;
+
+img{
+    border-radius: 2rem;
+}
+`;
 export default Popular;
